46b6498d55cfca4601cc7927e17802d4d1656648
GW <kowonp@gmail.com>
2022-02-16 16:20:17 +0900

    Set Styled components

---
+++ src/themes/GlobalStyle.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+
+  body{
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color:${(props) => props.theme.bgColor};
+    color:${(props) => props.theme.textColor};
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/themes/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "red",
+  textColor: "blue",
+  accentColor: "green",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "red",
+  textColor: "blue",
+  accentColor: "green",
+};
