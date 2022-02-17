0fa176a55cff1fb761dae5dfcf4edbdfc40fc8b2
GW <kowonp@gmail.com>
2022-02-18 01:41:54 +0900

    Loading Component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 20px;
+`;
+
+const Loading = () => {
+  return <Title>Loading...</Title>;
+};
+
+export default Loading;
