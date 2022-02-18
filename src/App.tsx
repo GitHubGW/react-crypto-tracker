9d8898ecfe02bcbd50568eed39b441275fb4651d
GW <kowonp@gmail.com>
2022-02-19 04:48:48 +0900

    Set Query Client

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import Router from "./Router";
 import GlobalStyle from "./themes/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./themes/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient: QueryClient = new QueryClient();
 
 const App = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
