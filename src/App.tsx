12b2bc12eb5b37c611d355c52ebafb7366f4687d
GW <kowonp@gmail.com>
2022-02-21 22:08:06 +0900

    Set React Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import GlobalStyle from "./themes/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./themes/theme";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { HelmetProvider } from "react-helmet-async";
 
 const queryClient: QueryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        <Router />
+        <HelmetProvider>
+          <GlobalStyle />
+          <Router />
+        </HelmetProvider>
       </ThemeProvider>
     </QueryClientProvider>
   );
